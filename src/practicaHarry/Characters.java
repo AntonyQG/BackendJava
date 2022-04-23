a4202c4898fe536466ef0e39194a28555cbc9162
Antony Quintero <antonyquinterog@gmail.com>
2022-04-23 16:19:33 -0500

    First commit and prepare the class

---
+++ src/practicaHarry/Characters.java
@@ -0,0 +1,10 @@
+package practicaHarry;
+
+/*Los personajes que se van a modelar son Harry, Hermione, Ron, Ginny y Albus*/
+/*
+* Catacteristicas para el modelado:
+* Nombre, Genero, Cumpleaños, Sangre (Si es mestizo, nacido de muggles, etc),familia, boggart, patronus, casa
+* */
+
+public class Characters {
+}

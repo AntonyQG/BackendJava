43b85fcdaf39a08aff5c98554998c10864abbf33
Antony Quintero <antonyquinterog@gmail.com>
2022-04-24 14:24:28 -0500

    Adding package fot the new practice and class Octocat

---
+++ src/practicaMona/Octocat.java
@@ -0,0 +1,12 @@
+package practicaMona;
+
+/*
+* Para definir a Octocat, que es la clase padre, como caracteristicas (atributos) el nombre, la vestimenta y sus actividades
+* Se tendran atributos, constructor, metodos para actividades
+* */
+
+public class Octocat {
+    private String nombre, vestimenta, actividad;
+
+
+}

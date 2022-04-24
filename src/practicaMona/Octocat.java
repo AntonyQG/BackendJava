1dd9da392783ab89d13a215e976441032906f23e
Antony Quintero <antonyquinterog@gmail.com>
2022-04-24 14:39:02 -0500

    Define class Octocat

--- src/practicaMona/Octocat.java
+++ src/practicaMona/Octocat.java
@@ -2,11 +2,31 @@ package practicaMona;
 
 /*
 * Para definir a Octocat, que es la clase padre, como caracteristicas (atributos) el nombre, la vestimenta y sus actividades
-* Se tendran atributos, constructor, metodos para actividades
+* Se tendran atributos, constructor, metodos para presentarse
 * */
 
 public class Octocat {
+    // Atributos
     private String nombre, vestimenta, actividad;
 
+    // Metodos
+    public void presentar(String mensaje) {
+        System.out.println("Hola desde la case padre Octocat");
+    }
 
+    // Cosntructores
+    public Octocat() {}
+
+    public Octocat(String nombre, String vestimenta, String actividad) {
+        this.nombre = nombre;
+        this.vestimenta = vestimenta;
+        this.actividad = actividad;
+    }
+
+    // Getters
+    public String getNombre() { return nombre; }
+
+    public String getVestimenta() { return vestimenta; }
+
+    public String getActividad() { return actividad; }
 }
